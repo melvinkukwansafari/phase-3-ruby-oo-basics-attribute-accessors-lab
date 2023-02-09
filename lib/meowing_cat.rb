fee99e818edca1f4a1665f3053b4642e2e78c8a7
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-02-09 14:09:37 +0300

    complete assigment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,24 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :cat_name
+    attr_reader :first_name, :last_name
+
+
+  def meow
+    puts "meow!"
+  end
+    
+   def name=(name)
+    
+    @name = name
+
+   end
+
+   def name
+    @name
+   end
+
+end
+
+maru = Cat.new
+maru.name = "Maru"
